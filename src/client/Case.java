4266d1faac82da4a955d84731a63869e3fa6246b
Marie-Donnie <delavergnemarie@gmail.com>
2017-02-02 19:04:44 +0100

    Ajout getters/setters

--- src/client/Case.java
+++ src/client/Case.java
@@ -3,4 +3,18 @@ package client;
 public class Case{
 	String nom;
 	int action;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getAction() {
+		return action;
+	}
+	public void setAction(int action) {
+		this.action = action;
+	}
+	
+	
 }
--- src/client/Map.java
+++ src/client/Map.java
@@ -2,17 +2,42 @@ package client;
 
 public class Map {
 	private Case[][] carte;
+	private int largeur;
+	private int hauteur;
 	
+	public Map(int largeur, int hauteur){
+		this.carte = new Case[largeur][hauteur];
+		this.largeur = largeur;
+		this.hauteur = hauteur;
+	}
+	
+	
+	public int getLargeur() {
+		return largeur;
+	}
+
+
+	public void setLargeur(int largeur) {
+		this.largeur = largeur;
+	}
+
+
+	public int getHauteur() {
+		return hauteur;
+	}
+
+
+	public void setHauteur(int hauteur) {
+		this.hauteur = hauteur;
+	}
+
+
 	public Case[][] getCarte() {
 		return carte;
 	}
 
 	public void setCarte(Case[][] carte) {
 		this.carte = carte;
-	}
-
-	public Map(int largeur, int hauteur){
-		this.carte = new Case[largeur][hauteur];
 	}
 	
 	public Case getCase(int x, int y){
