5145354461db5e4230b4f70c65172f5be6967d0e
Marie-Donnie <delavergnemarie@gmail.com>
2017-02-02 18:43:51 +0100

    Ajout main et map

---
+++ src/client/Main.java
@@ -0,0 +1,13 @@
+package client;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Personnage principal = new Personnage(10,2,"Robert",0,0);
+		Map map = new Map(3,3);
+
+	}
+
+}
--- src/client/Map.java
+++ src/client/Map.java
@@ -3,6 +3,10 @@ package client;
 public class Map {
 	private Case[][] carte;
 	
+	public Map(int largeur, int hauteur){
+		this.carte = new Case[largeur][hauteur];
+	}
+	
 	public Case getCase(int x, int y){
 		return carte[x][y];
 	}
