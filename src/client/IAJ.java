7d4fa7f24cfb5c59191708ca0e4b4bd2f36e5938
Sullivan PINEAU <E124533M@S046V7pc08.ensinfo.sciences.univ-nantes.prive>
2017-02-02 18:35:44 +0100

    av

---
+++ src/client/IAJ.java
@@ -0,0 +1,5 @@
+package client;
+
+public interface IAJ {
+	void action();
+}
---
+++ src/client/Jeu.java
@@ -0,0 +1,15 @@
+package client;
+
+public class Jeu {
+	Personnage hero;
+	Map map;
+
+	public Jeu(Personnage hero, Map map) {
+		super();
+		this.hero = hero;
+		this.map = map;
+	}
+	
+	public void action(){
+	}
+}
