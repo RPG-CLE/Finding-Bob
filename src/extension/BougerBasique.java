448acfca8b9a4f599e86d583a3b3c8b5b0106198
E126990H <E126990H@S046V7pc07.ensinfo.sciences.univ-nantes.prive>
2017-02-02 17:40:26 +0100

    first class of extension

---
+++ src/extension/BougerBasique.java
@@ -0,0 +1,23 @@
+package extension;
+import client.IBouger;
+import client.Personnage;
+public class BougerBasique implements IBouger {
+
+	public void bouger(String direction,Personnage perso) {
+		  
+	        
+	        switch (direction) {
+	            case "haut":  //TODO;
+	            	break;
+	            case "bas":  //TODO;
+		            break;
+	            case "gauche":  //TODO;
+		            break;
+	            case "droite":  //TODO;
+		            break;
+	            default: //TODO invalide direction;
+                break;
+	        }
+	}
+
+}
