fd6366d5d9d590688b267193dbb0010f66b06f7f
Marie-Donnie <delavergnemarie@gmail.com>
2017-02-02 18:07:21 +0100

    Added files

--- src/client/Map.java
+++ src/client/Map.java
@@ -1,9 +1,12 @@
 package client;
 
-import java.util.ArrayList;
+import java.util.ArrayList;;
 
 
 public class Map {
-
 	private ArrayList<ArrayList<Case>> carte;
+	
+	public Case getCase(int x, int y){
+		return carte.get(x).get(y);		
+	}
 }
