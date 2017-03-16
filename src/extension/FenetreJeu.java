54a9bb9a0dce3731487dded613a6eae7ba3a3a4f
B-Sarah <sboutahlil@gmail.com>
2017-03-16 17:11:03 +0100

    ajout fenetre jeu

---
+++ src/extension/FenetreJeu.java
@@ -0,0 +1,20 @@
+package extension;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class FenetreJeu extends JFrame implements ActionListener{
+
+	public FenetreJeu(String nomJeu) {
+		super(nomJeu);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
