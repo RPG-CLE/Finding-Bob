cc09c59d71c0b0bb74676515fc7ab4c9f4b9da5b
monta <montalvo@orange.fr>
2017-03-30 14:29:40 +0200

    Javadoc IAfficheur

--- src/client/interfaces/IAfficheur.java
+++ src/client/interfaces/IAfficheur.java
@@ -1,9 +1,20 @@
 package client.interfaces;
 
 import client.Jeu;
-
+/**
+ * Interface IAfficheur qui s'occupe de l'affichage graphique de l'application.
+ *  
+ * @author Araya, Boutahlil, Delavergne, Donnart, Pineau, Vall√©e
+ *
+ */
 public interface IAfficheur {
 
+	/**
+	 * Affiche l'Ètat actuel du jeu
+	 *  
+	 * @param jeu : le jeu ‡ afficher
+	 *
+	 */
 	void afficher(Jeu jeu);
 
 }
