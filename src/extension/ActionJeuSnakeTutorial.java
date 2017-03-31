65861d813c64a0e217a5b04cb3bf16c0ecf30344
Sullivan PINEAU <pineau.sullivan@gmail.com>
2017-03-31 18:36:37 +0200

    Master : ajout de commentaire extension Snake

--- src/extension/ActionJeuSnakeTutorial.java
+++ src/extension/ActionJeuSnakeTutorial.java
@@ -8,15 +8,22 @@ import client.Jeu;
 import client.interfaces.IAJ;
 import client.interfaces.IPersonnage;
 
-public class ActionJeuSnakeTutorial implements IAJ {
-	long lastTurn;
-	
-	public void ActionJeu() {
-		lastTurn = System.currentTimeMillis();
-	}
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////CLASSE EXPLIQUEE DANS LE TUTORIAL DU RAPPORT////////////////
+//////CF section du rapport : Implementation de l’interface ”IAJ"///////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Classe ActionJeuSnakeTutorial. Classe implémentent l'interface IAJ 
+ *  
+ * @author Araya, Boutahlil, Delavergne, Donnart, Pineau, Vallée
+ *
+ */
+public class ActionJeuSnakeTutorial implements IAJ {
 	@Override
 	public boolean action(Jeu j) {
+		//Sleep permetant de laisser le temps à l'utilisateur pour saisir une nouvelle direction
 		try {
 			TimeUnit.MILLISECONDS.sleep(400);
 		} catch (InterruptedException e) {
@@ -26,30 +33,36 @@ public class ActionJeuSnakeTutorial implements IAJ {
 
 		Evenement evt = j.getEntree().lireEntree();
 		if (evt != null){
+			//l'tilisateur a saisit une nouvelle direction alors appel de l'action approprié
 			j.getHero().doAction(ActionDeplacer.class, evt);
 		}else{
+			//l'tilisateur n'a pas saisit de nouvelle direction alors set la vie du snake à 99 et appelle la fonction déplacerDroite (appel par défaut) 
 			j.getHero().setPv(99);
 			j.getMap().deplacerDroite(j.getHero());
+			//Réinitialise la vie du snake à la normal s'il n'est pas mort
 			if(j.getHero().getPv()>0){
 				j.getHero().setPv(10);
 			}
 		}
 		
-		
-		
 		IPersonnage ennemi= (IPersonnage) j.getMap().getEnnemi(j.getHero().getPosX(), j.getHero().getPosY());
 		
 		if (ennemi!=null) {
+			//Le Snake mange un bonbon 
 			System.out.println("Miam");
 			Jeu.map.removePersonnage(ennemi);
-			double vie_hero = j.getHero().getPv();
 			Jeu.map.getCase(j.getHero().getPosX(), j.getHero().getPosY()).setPersonnage(j.getHero());
 		}
+
+		//analyse si le snake est mort
 		if (j.getHero().getPv() < 1){
+			//set le gameOn a false du jeu
 			j.setGameOn(false);
 		}
 
+		//affiche le jeu
 		j.afficher();
+		//return l'état du jeu (si return false alors l'application reconnaitra la fin de la partie)
 		return j.getGameOn();
 	}
 }
--- src/extension/MapSnakeTutorial.java
+++ src/extension/MapSnakeTutorial.java
@@ -12,30 +12,43 @@ import java.util.Random;
 
 import client.Case;
 import client.Evenement;
-import client.Jeu;
-import client.Personnage;
 import client.interfaces.IMap;
 import client.interfaces.IPersonnage;
 import framework.PartieProvider;
 
-public class MapSnakeTutorial implements IMap{
-	IPersonnage heros;
-	IPersonnage vilain;
-	private Case[][] cases;
-	private int largeur=20;
-	private int hauteur=20;
-	int positionPersonageY;
-	int positionPersonageX;
-	List<IPersonnage> ennemis;
-	private LinkedList<Integer> corpsMonstreX;
-	private LinkedList<Integer> corpsMonstreY;
-	private boolean up = false;
-	private IPersonnage partieCorps;
-	Evenement dernierEvt = new Evenement(Evenement.DROITE);
+////////////////////////////////////////////////////////////////////////////////
+////////////////////CLASSE EXPLIQUEE DANS LE TUTORIAL DU RAPPORT////////////////
+//////CF section du rapport : Implémentation de l’interface ”IMap"//////////////
+////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * Classe MapSnakeTutorial. Classe implémentent l'interface IMap 
+ *  
+ * @author Araya, Boutahlil, Delavergne, Donnart, Pineau, Vallée
+ *
+ */
+public class MapSnakeTutorial implements IMap{
+	private IPersonnage heros; // qui represente la tête de votre serpent
+	private Case[][] cases; // qui représente les différentes cases de votre map
+	private int largeur=20;  // qui représente la largeur de votre map
+	private int hauteur=20; // qui représente la hauteur de votre map
+	private List<IPersonnage> ennemis;// qui représente la liste des différents bonbons, dans notre démonstration, nous en auront qu'un seul
+	private LinkedList<Integer> corpsMonstreX; // qui représente le plus simplement une liste chainée des positions x du corps
+	private LinkedList<Integer> corpsMonstreY; // qui représente le plus simplement une liste chainée des positions y du corps
+	private boolean up = false; // qui représente lorsque le snake level up (mange un bonbon)
+	private IPersonnage partieCorps; // qui servira d'apparence pour le corps
+	private Evenement dernierEvt = new Evenement(Evenement.DROITE); // qui représente le déplacement par défaut (dernier déplacement initialisé à droite)
+	
+	/**
+	 * Constructeur de la classe MapSnakeTutorial
+	 * 
+	 * @return MapSnakeTutorial : la MapSnakeTutorial construite avec :
+	 * - ses différentes cases initialisé en herbe (et le contour en Terre)
+	 * - initialisation de ses deux listes chainées
+	 * - initialisation de sa partieCorps par l'appel de la fonction getPartieQueux
+	 * - l'appel de la fonction initialiseEnnemis
+	 */
 	public MapSnakeTutorial(){
-		this.positionPersonageY=2;
-		this.positionPersonageX=2;
 		this.cases = new Case[largeur][hauteur];
 		for (int i = 0; i < this.getLargeur(); i++) {
 			for (int j = 0; j < this.getHauteur(); j++) {
@@ -50,10 +63,37 @@ public class MapSnakeTutorial implements IMap{
 		this.corpsMonstreY = new LinkedList<Integer>() ;
 		
 		ennemis = new ArrayList<IPersonnage>();
-		partieCorps = getPartieQueux(0,0);
+		partieCorps = getPartieQueux();
 		initialiseEnnemis();
 	}
 	
+	/**
+	 * Permet de retourner un objet de type IPersonnage lié à la configuation 
+	 * 
+	 * @return IPersonnage : représentant une partie du corps du snake (Qui sera utilisé pour son apparence)
+	 */
+	private IPersonnage getPartieQueux(){
+		Properties prop = new Properties();
+		IPersonnage partieQueux = null;
+		try {
+			prop.load(new FileReader("src/extension/configMap/configSnakeTutorial.txt"));
+			for(Object key : prop.keySet()){
+				if(key.toString().equals("queuxsnake")){
+					partieQueux = (IPersonnage) (IPersonnage) PartieProvider.getInstance().getObjetByConfig(IPersonnage.class,
+							(String)prop.get(key));
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return partieQueux;
+	}
+	
+	/**
+	 *  Initialise les différents ennemis (dans cette extension, la liste possède un ennemi = un bonbon) lié à la configuration
+	 */
 	private void initialiseEnnemis(){
 		Properties prop = new Properties();
 		try {
@@ -66,68 +106,100 @@ public class MapSnakeTutorial implements IMap{
 					this.getCase(ennemi.getPosX(), ennemi.getPosY()).setPersonnage(ennemi);		
 				}
 			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	private IPersonnage getPartieQueux(int x, int y){
-		Properties prop = new Properties();
-		IPersonnage partieQueux = null;
-		try {
-			prop.load(new FileReader("src/extension/configMap/configSnakeTutorial.txt"));
-			for(Object key : prop.keySet()){
-				if(key.toString().equals("queuxsnake")){
-					partieQueux = (IPersonnage) (IPersonnage) PartieProvider.getInstance().getObjetByConfig(IPersonnage.class,
-							(String)prop.get(key));
-				}
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return partieQueux;
-	}
-	
+	/**
+	 * Accesseur de l'attribut largeur
+	 * 
+	 * @return int : la largeur de la map
+	 */
 	public int getLargeur() {
 		return largeur;
 	}
 
-
+	/**
+	 * Permet de retourner un objet de type IPersonnage lié à sa configuation 
+	 * 
+	 * @return IPersonnage : représentant une partie du corps du snake (Qui sera utilisé pour son apparence)
+	 */
 	public void setLargeur(int largeur) {
 		this.largeur = largeur;
 	}
 
-
+	/**
+	 * Getter de l'attribut hauteur
+	 * 
+ 	 * @return int : hauteur 
+	 */
 	public int getHauteur() {
 		return hauteur;
 	}
 
-
+	/**
+	 * Setter de l'attribut hauteur de la classe
+	 * 
+	 * @param hauteur : la nouvelle haute
+	 */
 	public void setHauteur(int hauteur) {
 		this.hauteur = hauteur;
 	}
 
+	/**
+	 * Getter de l'attribut ennemis
+	 * 
+ 	 * @return List<IPersonnage> : ennemis 
+	 */
+	public List<IPersonnage> getEnnemis() {
+		return ennemis;
+	}
+	
+	/**
+	 * Setter de l'attribut ennemis de la classe
+	 * 
+	 * @param ennemis : la nouvelle liste des ennemis
+	 */
+	public void setEnnemis(List<IPersonnage> ennemis) {
+		this.ennemis = ennemis;
+	}
 
+	/**
+	 * Accesseur de l'attribut cases
+	 * 
+	 * @return Case[][] : le tableau de tableau des différentes cases
+	 */
 	public Case[][] getCases() {
 		return cases;
 	}
 
+	/**
+	 * Setter de l'attribut cases de la classe
+	 * 
+	 * @param Case[][] : la nouvelle carte
+	 */
 	public void setCases(Case[][] carte) {
 		this.cases = carte;
 	}
 	
+	/**
+	 * Setter de la case dans l'attribut cases en position x,y de la classe
+	 * 
+	 * @param x : Coordonnée x 
+	 * @param y : Coordonnée y 
+	 * @param _case : Coordonnée _case
+	 */
 	public void setCase(int x, int y, Case _case){
 		cases[x][y] = _case;
 	}
 	
+	/**
+	 * Accesseur de la case en position x,y dans l'attribut cases[][]
+	 * 
+	 * @return Case : case correspondant aux coordonnées x,y, null si elle n'existe pas (Hors limite)
+	 */
 	public Case getCase(int x, int y){
 		if(x >= 0 && y >= 0 && x < getLargeur() && y < getHauteur())
 			return cases[x][y];
@@ -135,6 +207,11 @@ public class MapSnakeTutorial implements IMap{
 			return null;
 	}
 	
+	/**
+	 * Accesseur du personnage de la case en position x,y dans l'attribut cases[][]
+	 * 
+	 * @return IPersonnage : Ipersonnage se trouvant sur la case correspondant aux coordonnées x,y, null si elle n'existe pas (Hors limite)
+	 */
 	public IPersonnage getEnnemi(int x, int y){
 		for(IPersonnage pers : ennemis){
 			if(pers.getPosX()==x && pers.getPosY() == y){
@@ -144,58 +221,21 @@ public class MapSnakeTutorial implements IMap{
 		return null;
 	}
 	
-	public List<Case> getCasesAdjacentes(int x, int y){
-		List<Case> listCase = new ArrayList<Case>();
-		
-		if(this.getCase(x, y-1) != null)
-			listCase.add(this.getCase(x, y-1));
-		if(this.getCase(x, y+1) != null)
-			listCase.add(this.getCase(x, y+1));
-		if(this.getCase(x-1, y) != null)
-			listCase.add(this.getCase(x-1, y));
-		if(this.getCase(x+1, y) != null)
-			listCase.add(this.getCase(x+1, y));
-		
-		if(this.getCase(x-1, y-1) != null)
-			listCase.add(this.getCase(x-1, y-1));
-		if(this.getCase(x+1, y-1) != null)
-			listCase.add(this.getCase(x+1, y-1));
-		if(this.getCase(x+1, y+1) != null)
-			listCase.add(this.getCase(x+1, y+1));
-		if(this.getCase(x-1, y+1) != null)
-			listCase.add(this.getCase(x-1, y+1));
-				
-		return listCase;
-	}
-	
-
-	public int getPositionPersonageX(){
-		return heros.getPosX();
-	}
-	public int getPositionPersonageY(){
-		return heros.getPosY();
-	}
-	
-	public void setPositionPersonageX(int valeur){
-		positionPersonageX=valeur;
-	}
-	
-	public void setPositionPersonageY(int valeur){
-		positionPersonageY=valeur;
-	}
-	
+	/**
+	 * Setter du heros et set le personnage à la bonne case 
+	 *  
+	 * @param heros : nouveau heros (snake)
+	 */
 	public void setHero(IPersonnage heros){
 		this.heros = heros;
-		heros.setPosX(positionPersonageX);
-		heros.setPosY(positionPersonageY);
 		this.getCase(heros.getPosX(), heros.getPosY()).setPersonnage(heros);
 	}
 	
-	public void setVilain(IPersonnage vilain){
-		this.vilain = vilain;
-		this.getCase(vilain.getPosX(), vilain.getPosY()).setPersonnage(vilain);
-	}
-	
+	/**
+	 * Gère le déplacement vers la droite du snap sur la map
+	 * 
+	 * @param IPersonnage : perso qui représentera le snake (héros)
+	 */
 	public void deplacerDroite(IPersonnage perso){
 		int xavant = perso.getPosX();
 		int yavant = perso.getPosY();
@@ -227,6 +267,12 @@ public class MapSnakeTutorial implements IMap{
 		seMangeOuMur(perso);
 		actualise(xavant,yavant);
 	}
+	
+	/**
+	 * Gère le déplacement vers la gauche du snap sur la map
+	 * 
+	 * @param IPersonnage : perso qui représentera le snake (héros)
+	 */
 	public void deplacerGauche(IPersonnage perso){
 		this.dernierEvt = new Evenement(Evenement.GAUCHE);
 		int xavant = perso.getPosX();
@@ -239,6 +285,12 @@ public class MapSnakeTutorial implements IMap{
 		seMangeOuMur(perso);
 		actualise(xavant,yavant);
 	}
+	
+	/**
+	 * Gère le déplacement vers le haut du snap sur la map
+	 * 
+	 * @param IPersonnage : perso qui représentera le snake (héros)
+	 */
 	public void deplacerHaut(IPersonnage perso){
 		this.dernierEvt = new Evenement(Evenement.HAUT);
 		int xavant = perso.getPosX();
@@ -251,6 +303,12 @@ public class MapSnakeTutorial implements IMap{
 		seMangeOuMur(perso);
 		actualise(xavant,yavant);
 	}
+	
+	/**
+	 * Gère le déplacement vers le bas du snap sur la map
+	 * 
+	 * @param IPersonnage : perso qui représentera le snake (héros)
+	 */
 	public void deplacerBas(IPersonnage perso){
 		this.dernierEvt = new Evenement(Evenement.BAS);
 		int xavant = perso.getPosX();
@@ -264,22 +322,20 @@ public class MapSnakeTutorial implements IMap{
 		actualise(xavant,yavant);
 	}
 	
+	/**
+	 * Procédure permettant de detecter des collisions (corps ou mur) et donc de tuer le snake(héros)
+ 	 * @param perso : IPersonnage
+	 */
 	private void seMangeOuMur(IPersonnage perso){
 		if(!dispoCase(perso.getPosX(), perso.getPosY())){
 			perso.setPv(0);
 		}
 	}
-	private void genererMonde(double pourcentage) {
-	}
 	
-	public List<IPersonnage> getEnnemis() {
-		return ennemis;
-	}
-
-	public void setEnnemis(List<IPersonnage> ennemis) {
-		this.ennemis = ennemis;
-	}
-
+	/**
+	 * Procédure permetant génèrer une nouvelle position possible pour le bonbon et setPersonnage de l'ancienne case à null
+ 	 * @param personnage : IPersonnage
+	 */
 	@Override
 	public void removePersonnage(IPersonnage personnage){
 		this.up=true;
@@ -301,7 +357,13 @@ public class MapSnakeTutorial implements IMap{
 		this.getCase(caseX,caseY).setPersonnage(ennemis.get(0));
 	}
 	
-	
+	/**
+	 * Fonction permetant de détecter des collisions (Mur, bordure et corps du snake) en position caseX et caseY
+	 * 
+ 	 * @param caseX : int
+ 	 * @param caseY : int
+ 	 * @return boolean : true si la case est disponible, false sinon
+	 */
 	private boolean dispoCase(int caseX, int caseY){
 		int posx = 0;
 		int posy = 0;
@@ -320,6 +382,14 @@ public class MapSnakeTutorial implements IMap{
 		return true;
 	}
 	
+	/**
+	 * Procédure permet d'actualiser la longueur de la queux (corps) du snake 
+	 * Si up == true alors ajouter la position du héros sinon supprimer la queux et ajouter la position du héros
+	 * Cet appel de procédure est appelé dans chaque deplacement
+	 * 
+ 	 * @param x : int
+ 	 * @param y : int
+	 */
 	private void actualise(int x, int y){
 		if(!this.up){
 			if(this.corpsMonstreX.size()>0){
@@ -342,9 +412,41 @@ public class MapSnakeTutorial implements IMap{
 
 	}
 
+	
+	/////////////////////////////////////////////////////////////////////////
+	/////Fonctions inutiles d'implémenter pour cette extension///////////////
+	/////////////////////////////////////////////////////////////////////////
+	
 	@Override
 	public boolean rechargementTexture() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	public List<Case> getCasesAdjacentes(int x, int y){
+		List<Case> listCase = new ArrayList<Case>();
+		//inutile dans cette extension
+		return listCase;
+	}
+
+	public int getPositionPersonageX(){
+		//inutile dans cette extension
+		return 0;
+	}
+	public int getPositionPersonageY(){
+		//inutile dans cette extension
+		return 0;
+	}
+	
+	public void setPositionPersonageX(int valeur){
+		//inutile dans cette extension
+	}
+	
+	public void setPositionPersonageY(int valeur){
+		//inutile dans cette extension
+	}
+	
+	public void setVilain(IPersonnage vilain){
+		//Pas besoin d'implémenter
 	}
 }
